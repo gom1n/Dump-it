b3aa7371c55c74333be3feb11c10a4d7b237d618
gom1n <alice436777@gmail.com>
2021-09-02 00:51:50 +0900

    gomin_commit12_검색기능오류해결

--- app/src/main/java/com/dumpit/ffff/Home.java
+++ app/src/main/java/com/dumpit/ffff/Home.java
@@ -1,5 +1,6 @@
 package com.dumpit.ffff;
 
+import android.app.Dialog;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -9,12 +10,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.kakao.plusfriend.PlusFriendService;
 import com.kakao.util.exception.KakaoException;
 
@@ -23,13 +29,27 @@ import java.security.NoSuchAlgorithmException;
 
 public class Home extends Fragment{
     ViewGroup viewGroup;
+    Dialog dialog;
+    TextView HowToUse;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState)
-    {
+                             @Nullable Bundle savedInstanceState) {
 
         viewGroup = (ViewGroup) inflater.inflate(R.layout.home,container,false);
+
+        dialog = new Dialog(this.getContext());       // Dialog 초기화
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dialog.setContentView(R.layout.help_dialog);             // xml 레이아웃 파일과 연결
+
+        HowToUse = (TextView) viewGroup.findViewById(R.id.howToUse);
+        HowToUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
 
         return viewGroup;
     }
